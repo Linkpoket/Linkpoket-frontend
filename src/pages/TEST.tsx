bc9e6d7da82b14e200a4b66ab535e93456e1e2fd
hoonshi <qkekxovnd@hufs-gsuite.kr>
2025-04-06 18:21:37 +0900

    feat: 공통컴포넌트 viewtoggle 제작 및 버튼컴포넌트 white에서gray0으로 변경

--- src/pages/TEST.tsx
+++ src/pages/TEST.tsx
@@ -1,22 +1,3 @@
-import { Button } from '@/shared/ui/Button';
-
 export default function TEST() {
-  return (
-    <div>
-      <h1 className="mx-auto w-[300px]">테스트를 위한 임시 파일입니다.</h1>
-      <div className="flex item-center justify-center gap-2 my-20">
-        <Button variant="primary" size="lg" className="py-2">
-          버튼
-        </Button>
-
-        <Button variant="secondary" size="lg" className="py-2">
-          버튼
-        </Button>
-
-        <Button variant="ghost" size="lg" className="py-2">
-          버튼
-        </Button>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
