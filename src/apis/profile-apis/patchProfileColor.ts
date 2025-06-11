3ca36f13bb5884d279c3bd78830f913042805dfc
cyweyo <cyw7715@gmail.com>
2025-06-12 00:53:32 +0900

    fix: 불필요한 try/catch 제거 및 오류 타입 지정

--- src/apis/profile-apis/patchProfileColor.ts
+++ src/apis/profile-apis/patchProfileColor.ts
@@ -9,12 +9,8 @@ interface PatchProfileColorResponse {
 export const patchProfileColor = async (
   colorCode: string
 ): Promise<PatchProfileColorResponse> => {
-  try {
-    const response = await axiosInstance.patch('/api/member/color-code', {
-      colorCode,
-    });
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await axiosInstance.patch('/api/member/color-code', {
+    colorCode,
+  });
+  return response.data;
 };
