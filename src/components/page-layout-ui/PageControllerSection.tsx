3c69ed3522b084b0df2b6aaf4df7d7d0e0d15023
hoonshi <qkekxovnd@hufs-gsuite.kr>
2025-07-13 00:21:04 +0900

    feat: 폴더/링크가 0개일 경우 기본값 0으로 처리

--- src/components/page-layout-ui/PageControllerSection.tsx
+++ src/components/page-layout-ui/PageControllerSection.tsx
@@ -2,8 +2,8 @@ import { PageControllerSectionProps } from '@/types/pages';
 import DropDownView from '../common-ui/DropDownView';
 
 export default function PageControllerSection({
-  folderDataLength,
-  linkDataLength,
+  folderDataLength = 0,
+  linkDataLength = 0,
 }: PageControllerSectionProps) {
   return (
     <div className="mb-2 flex h-[42px] items-center justify-between">
