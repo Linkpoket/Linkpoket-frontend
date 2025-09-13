bf15f0b29b29834d249f58c144a3c61c5d0beff3
hoonshi <qkekxovnd@hufs-gsuite.kr>
2025-09-13 16:29:09 +0900

    feat: 폴더,링크 업데이트시 캐싱 조건부를 통한 처리

--- src/hooks/mutations/useUpdateFolder.ts
+++ src/hooks/mutations/useUpdateFolder.ts
@@ -5,12 +5,19 @@ import {
 } from '@tanstack/react-query';
 import updateFolder from '@/apis/folder-apis/updateFolder';
 import { UpdateFolderData } from '@/types/folders';
+import { useLocation } from 'react-router-dom';
 
 export default function useUpdateFolder(
   pageId: string,
   options?: UseMutationOptions<any, unknown, UpdateFolderData>
 ) {
   const queryClient = useQueryClient();
+  const location = useLocation();
+  const locationSplit = location.pathname.split('/');
+  const isMainPage = location.pathname === '/';
+  const isBookmarksPage = location.pathname === '/bookmarks';
+  const isSharedPage = locationSplit.includes('shared');
+  const isFolderPage = locationSplit.includes('folder');
 
   return useMutation({
     ...options,
@@ -19,28 +26,36 @@ export default function useUpdateFolder(
       console.log('폴더 업데이트 성공 응답:', response);
 
       // 폴더 상세, 공유페이지, 개인페이지 캐시 무효화
-      Promise.allSettled([
+      if (isFolderPage) {
         queryClient.invalidateQueries({
           queryKey: ['folderDetails', variables.baseRequest.pageId],
-        }),
+        });
+      }
+      if (isFolderPage) {
         queryClient.invalidateQueries({
           queryKey: ['folderList', pageId],
           refetchType: 'active',
-        }),
+        });
+      }
+      if (isSharedPage) {
         queryClient.invalidateQueries({
           queryKey: ['sharedPage', pageId],
-        }),
+        });
+      }
+      if (isMainPage) {
         queryClient.invalidateQueries({
           queryKey: ['personalPage'],
-        }),
+        });
+      }
+      if (isBookmarksPage) {
         queryClient.invalidateQueries({
           queryKey: ['favorite'],
           refetchType: 'active',
-        }),
-      ]);
+        });
 
-      if (options?.onSuccess) {
-        options.onSuccess(response, variables, context);
+        if (options?.onSuccess) {
+          options.onSuccess(response, variables, context);
+        }
       }
     },
     onError: (error, variables, context) => {
