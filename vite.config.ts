d689d257d92a11c91bb9a12364bc7ba3358b23c4
hoonshi <qkekxovnd@hufs-gsuite.kr>
2025-09-21 02:47:26 +0900

    feat: vite 빌드 최적화 - vender를 통해 변경되지 않는 라이브러리를 하나로 묶고, ui 컴포넌를 별도의 파일로 묶음 - terser를 통한 코드 압축 - 기본 파일 압축 및 console, debugger문 빌드시 제거

--- vite.config.ts
+++ vite.config.ts
@@ -40,18 +40,44 @@ export default defineConfig({
         extension: resolve(__dirname, 'src/extension/main-extension.tsx'),
       },
       output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: 'assets/[name].[hash].js',
+        //파일 이름 지정
+        entryFileNames: 'assets/[name].[hash].js',
+        //청크 파일 이름 지정
+        chunkFileNames: 'assets/chunks/[name].[hash].js',
+        // 에셋 파일 분류
         assetFileNames: (assetInfo) => {
           if (assetInfo.name?.endsWith('.css')) {
-            return 'assets/global.css';
+            return 'assets/css/[name].[hash].css';
           }
-          return 'assets/[name].[ext]';
+          if (assetInfo.name?.match(/\.(woff2?|ttf|eot)$/)) {
+            return 'assets/fonts/[name].[hash][extname]';
+          }
+          if (assetInfo.name?.match(/\.(png|jpe?g|gif|svg|webp|ico)$/)) {
+            return 'assets/images/[name].[hash][extname]';
+          }
+          return 'assets/[name].[hash][extname]';
+        },
+        //성능 개선 유도 manualChunks
+        manualChunks: {
+          vendor: [
+            'react',
+            'react-dom',
+            'react-router-dom',
+            '@tanstack/react-query',
+          ],
+          'ui-components': ['@/components/common-ui', '@/components/modal'],
         },
       },
     },
     outDir: 'dist',
     assetsDir: 'assets',
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
   },
   resolve: {
     alias: {
